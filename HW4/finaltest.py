971815eee2360ee3cc903dd07dd31196f9472097
Benji8bit <coin8bit@hotmail.com>
2023-01-12 00:01:15 +0300

    Added echobot without TOKEN

---
+++ HW4/finaltest.py
@@ -0,0 +1,12 @@
+# dictionary = {}
+# key = "another_key"
+# value = "another_value"
+# dictionary["key"] = "value"
+# dictionary[key] = value
+# result = dictionary[key]
+
+x = 10
+while True:
+    if x % 2:
+        print(x)
+        x -= 1
