8784b58878e24dee4bad631d39b9ac2ec950d7e9
Benji8bit <coin8bit@hotmail.com>
2023-07-09 02:06:42 +0300

    Edited hw4_1 and finaltest

--- HW4/finaltest.py
+++ HW4/finaltest.py
@@ -4,9 +4,23 @@
 # dictionary["key"] = "value"
 # dictionary[key] = value
 # result = dictionary[key]
+# print (result)
+# print (dictionary)
 
 x = 10
 while True:
     if x % 2:
         print(x)
         x -= 1
+
+# x = 100
+# if x % 3:
+#             	print("CASE1")
+# elif x % 2:
+#             	print("CASE2")
+# elif x % 5:
+#             	print("CASE3")
+# elif x % 50:
+#             	print("CASE4")
+# else:
+#             	print("NOTHING")
